0569257e90687e9d4ccfea5674784333356ed6dd
indriHutabalian <indri.hutabalian@moonlay.com>
2017-11-06 18:20:14 +0700

    add router weekly plan

---
+++ routes/v1.js
@@ -0,0 +1,8 @@
+//WEEKLY PLAN
+var v1WeeklyPlanByUserRouter = require("../src/routers/v1/weekly-plan/weekly-plan-by-user-router");
+var v1WeeklyPlanRouter = require("../src/routers/v1/weekly-plan/weekly-plan-router");
+module.exports = function (server) {
+    //WEEKLY PLAN
+    v1WeeklyPlanByUserRouter().applyRoutes(server,  "/v1/weekly-plans/by-user");
+    v1WeeklyPlanRouter().applyRoutes(server,        "/v1/weekly-plans");
+}
