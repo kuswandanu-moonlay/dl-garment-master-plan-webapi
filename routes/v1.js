b517c7fb6356c7ac93d77d716439eceb90ed33e6
achensetyawati <achenzetya@gmail.com>
2017-11-11 11:50:51 +0700

    add working hours standard routes

--- routes/v1.js
+++ routes/v1.js
@@ -1,8 +1,12 @@
 //WEEKLY PLAN
 var v1WeeklyPlanByUserRouter = require("../src/routers/v1/weekly-plan/weekly-plan-by-user-router");
 var v1WeeklyPlanRouter = require("../src/routers/v1/weekly-plan/weekly-plan-router");
+var v1WorkingHoursStandardRouter = require("../src/routers/v1/working-hours-standard/working-hours-standard-router");
 module.exports = function (server) {
     //WEEKLY PLAN
-    v1WeeklyPlanByUserRouter().applyRoutes(server,  "/v1/weekly-plans/by-user");
-    v1WeeklyPlanRouter().applyRoutes(server,        "/v1/weekly-plans");
+    v1WeeklyPlanByUserRouter().applyRoutes(server,            "/v1/weekly-plans/by-user");
+    v1WeeklyPlanRouter().applyRoutes(server,                  "/v1/weekly-plans");
+
+    //WORKING HOURS STANDARD
+    v1WorkingHoursStandardRouter().applyRoutes(server,        "/v1/working-hours-standards");
 }
