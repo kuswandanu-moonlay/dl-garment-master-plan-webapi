a5388945e902e544906117b045e6bdf4b8a92bfb
achensetyawati <achenzetya@gmail.com>
2017-11-29 14:46:06 +0700

    routes

--- routes/default.js
+++ routes/default.js
@@ -1,13 +1,23 @@
 //WEEKLY PLAN
 var v1WeeklyPlanByUserRouter = require("../src/routers/v1/weekly-plan/weekly-plan-by-user-router");
 var v1WeeklyPlanRouter = require("../src/routers/v1/weekly-plan/weekly-plan-router");
+var v1WeeklyPlanByYearRouter = require("../src/routers/v1/weekly-plan/weekly-plan-by-year-router");
+
 var v1WorkingHoursStandardRouter = require("../src/routers/v1/working-hours-standard/working-hours-standard-router");
+
 var v1StyleRouter = require("../src/routers/v1/style/style-router");
+
 var v1StandardHourRouter = require("../src/routers/v1/standard-hour/standard-hour-router");
+var v1StandardHourByStyleRouter = require("../src/routers/v1/standard-hour/standard-hour-by-style-router");
+
+var v1BookingOrderRouter = require("../src/routers/v1/booking-order/booking-order-router");
+var v1BookingOrderPostRouter = require("../src/routers/v1/booking-order/booking-order-post-router");
+
 module.exports = function (server) {
     //WEEKLY PLAN
     v1WeeklyPlanByUserRouter().applyRoutes(server,  "/weekly-plans/by-user");
     v1WeeklyPlanRouter().applyRoutes(server,        "/weekly-plans");
+    v1WeeklyPlanByYearRouter().applyRoutes(server,  "/weekly-plans-by-year");
 
     //WORKING HOURS STANDARD
     v1WorkingHoursStandardRouter().applyRoutes(server,        "/working-hours-standards");
@@ -15,7 +25,11 @@ module.exports = function (server) {
     //STYLE
     v1StyleRouter().applyRoutes(server,        "/styles");
     
-    //STYLE
+    //STANDARD HOURS
     v1StandardHourRouter().applyRoutes(server,        "/standard-hours");
+    v1StandardHourByStyleRouter().applyRoutes(server,        "/standard-hours-by-style");
 
+    //BOOKING ORDER
+    v1BookingOrderRouter().applyRoutes(server,                       "/booking-orders");
+    v1BookingOrderPostRouter().applyRoutes(server,                   "/booking-orders-post");
 }
