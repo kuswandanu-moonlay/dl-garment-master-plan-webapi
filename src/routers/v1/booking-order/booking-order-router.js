ffaa4269efe78095f278196bca0851d23f829d15
achensetyawati <achenzetya@gmail.com>
2017-11-29 14:45:56 +0700

    add booking order router

---
+++ src/routers/v1/booking-order/booking-order-router.js
@@ -0,0 +1,14 @@
+const apiVersion = '1.0.0';
+var Manager = require("dl-module").managers.garmentMasterPlan.BookingOrderManager;
+var JwtRouterFactory = require("../../jwt-router-factory");
+
+function getRouter() {
+    var router = JwtRouterFactory(Manager, {
+        version: apiVersion,
+        defaultOrder: {
+            "_updatedDate": -1
+        }
+    });
+    return router;
+}
+module.exports = getRouter;
