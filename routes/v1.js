758ff755ea3348ddb5f1fa34bc541ba7ab5f622f
achensetyawati <achenzetya@gmail.com>
2017-12-04 13:26:58 +0700

    add master comodity

--- routes/v1.js
+++ routes/v1.js
@@ -13,6 +13,8 @@ var v1StandardHourByStyleRouter = require("../src/routers/v1/standard-hour/stand
 var v1BookingOrderRouter = require("../src/routers/v1/booking-order/booking-order-router");
 var v1BookingOrderPostRouter = require("../src/routers/v1/booking-order/booking-order-post-router");
 
+var v1MasterPlanComodityRouter = require("../src/routers/v1/master-plan-comodity/master-plan-comodity-router");
+
 module.exports = function (server) {
     //WEEKLY PLAN
     v1WeeklyPlanByUserRouter().applyRoutes(server,            "/v1/weekly-plans/by-user");
@@ -32,4 +34,7 @@ module.exports = function (server) {
     //BOOKING ORDER
     v1BookingOrderRouter().applyRoutes(server,                       "/v1/booking-orders");
     v1BookingOrderPostRouter().applyRoutes(server,                   "/v1/booking-orders-post");
+    
+    //MASTER PLAN COMODITY
+    v1MasterPlanComodityRouter().applyRoutes(server,                 "/v1/master-plan-comodities");
 }
